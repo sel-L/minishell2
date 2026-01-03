8648817177fd22fc8dfc2cc0775cf86e204bcf39
kiko <selenebboy@gmail.com>
2026-01-03 15:07:33 +0800

    Finished echo.c

---
+++ headers/sub_minishell.h
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   sub_minishell.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: selow <selow@student.42kl.edu.my>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/01/02 20:02:22 by selow             #+#    #+#             */
+/*   Updated: 2026/01/02 20:02:24 by selow            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+
+#ifndef SUB_MINISHELL_H
+#define SUB_MINISHELL_H
+
+#include "main_minishell.h"
+
+// builtins
+void	echo(char **argv);
+
+#endif
--- src/builtins/echo.c
+++ src/builtins/echo.c
@@ -3,11 +3,56 @@
 /*                                                        :::      ::::::::   */
 /*   echo.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: wshou-xi <wshou-xi@student.42kl.edu.my>    +#+  +:+       +#+        */
+/*   By: selow <selow@student.42kl.edu.my>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/12/14 13:22:15 by wshou-xi          #+#    #+#             */
-/*   Updated: 2025/12/14 13:22:16 by wshou-xi         ###   ########.fr       */
+/*   Created: 2025/12/29 15:21:57 by selow             #+#    #+#             */
+/*   Updated: 2025/12/29 15:21:59 by selow            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "sub_minishell.h"
+#include <stdbool.h>
 
+static bool	has_n_flag(char *str)
+{
+	if (!str)
+		return (false);
+	if (*str == '-')
+		str++;
+	else
+		return (false);
+	while (*str)
+	{
+		if (*str != 'n')
+			return (false);
+		str++;
+	}
+	return (true);
+}
+/*
+- print whatever is passed in
+- if -n flag is specified, print without a newline
+*/
+void	echo(char **argv)
+{
+	int		i;
+	bool	n_flag;
+
+	i = 1;
+	n_flag = has_n_flag(argv[i]);
+	if (n_flag)
+		i++;
+	while (argv[i])
+	{
+		ft_putstr_fd(argv[i++], 1);
+		if (argv[i])
+			ft_putchar_fd(' ', 1);
+	}
+	if (n_flag == false)
+		ft_putchar_fd('\n', 1);
+}
+/*
+int main(int argc, char **argv)
+{
+	echo(argv);
+}*/
