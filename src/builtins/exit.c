3bb1ac7842233cbea5a77cd620c8e3f18353fe63
Sel-L <selenebboy@gmail.com>
2026-01-20 17:59:57 +0800

    adding part of the exit() func, NOT COMPLETED YET

--- src/builtins/exit.c
+++ src/builtins/exit.c
@@ -3,10 +3,76 @@
 /*                                                        :::      ::::::::   */
 /*   exit.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: wshou-xi <wshou-xi@student.42kl.edu.my>    +#+  +:+       +#+        */
+/*   By: selow <selow@student.42kl.edu.my>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/12/14 13:22:25 by wshou-xi          #+#    #+#             */
-/*   Updated: 2025/12/14 13:22:26 by wshou-xi         ###   ########.fr       */
+/*   Created: 2025/12/20 02:19:19 by selow             #+#    #+#             */
+/*   Updated: 2025/12/20 02:57:01 by selow            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "sub_minishell.h"
+
+/*
+builtin exit 
+-> if any other command is called, do not print quit
+-> find exit code
+
+if exit_code not specified:
+	exit with 0
+
+Error handling
+if exit has too many arguments:
+	do not exit
+	Change exit_code ($?) to 1
+	And print “minishell: exit: too many arguments”
+
+If exit’s arguments has anything that arent numbers
+	Print exit
+	Change exit_code ($?) to 2
+	Print “minishell: exit: <command>: numeric argument required”
+	Exit
+*/
+
+void	ft_exit(char **argv, int argc)
+{
+	int	exit_code;
+
+	exit_code = find_exit_code();
+	if (argc > 2)
+	{
+		ft_putstr_fd("minishell: exit: too many arguments", 2);
+		exit_code = 1;
+		// change the env variable
+		return ; // ??
+	}
+	if (!is_num(argv[1]))
+	{
+		ft_putstr_fd("minishell: exit: ", 2);
+		write(2, argv[1], ft_strlen(argv[1]));
+		ft_putstr_fd(": numeric argument required", 2);
+		exit_code = 2;
+	}
+	exit_minishell(exit_code);
+}
+
+// checks if a string is completely integer characters, NO SPACES OR ANYTHING
+bool	is_num(char *str)
+{
+	while (*str)
+	{
+		if (*str < '0' && *str > '9')
+			return (false);
+		str++;
+	}
+	return (true);
+}
+
+void	exit_minishell()
+{
+	
+}
+
+int	find_exit_code()
+{
+
+}
